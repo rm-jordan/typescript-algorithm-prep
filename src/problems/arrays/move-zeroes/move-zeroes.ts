06a94f1a0d7b98212cd834397499f3758d82259a
Ryan Jordan <ryan_m_jordan@hotmail.com>
2026-05-18 11:08:07 -0400

    move zero completed

--- src/problems/arrays/move-zeroes/move-zeroes.ts
+++ src/problems/arrays/move-zeroes/move-zeroes.ts
@@ -18,6 +18,47 @@ Output:
 */
 
 export function moveZeroes(arr: number[]): number[] {
-  // your code here
-  return [];
+  // suggestion is collect all non zero values, add zeroes afterwards
+  const result: number[] = [];
+
+  // count zeroes
+  let zeroCount = 0;
+
+  // loop through array
+  for (const num of arr) {
+    // if non-zero:
+    if (num !== 0) {
+      result.push(num);
+    }
+    // push into result
+    if (num === 0) {
+      zeroCount++;
+    }
+    // otherwise:
+    console.log("zeroCount", zeroCount);
+    // increase zeroCount
+  }
+
+  // after loop:
+  for (let i = 0; i < zeroCount; i++) {
+    result.push(0);
+  }
+  // append zeroes to result
+  return result;
 }
+
+
+
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+// [1, 3, 12, 0, 0]
+// console.log(moveZeroes([0, 0, 1]));
+// // [1, 0, 0]
+// console.log(moveZeroes([1, 2, 3]));
+// // [1, 2, 3]
+// console.log(moveZeroes([0, 0, 0]));
+// // [0, 0, 0]
+// console.log(moveZeroes([]));
+// // []
+// console.log(moveZeroes([4, 0, 5, 0, 0, 3]));
+// // [4, 5, 3, 0, 0, 0]
